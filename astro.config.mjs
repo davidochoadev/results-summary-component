3aebf764622d3c64d9323156ef5aed333f56c9e8
davideochoaa <davideochoaa@gmail.com>
2023-06-11 19:15:04 +0200

    ✅ Feat: Imported Front-end mentor files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://davidochoadev.github.io',
-   base: '/results-summary-component',
+  site: 'https://davidochoadev.github.io',
+  base: '/results-summary-component',
+  integrations: [react(), tailwind()]
 });
